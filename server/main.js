f92b2e830e3707a0500f9d4f721be6ff700e4c54
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2016-04-10 11:29:08 -0400

    cleanup

--- server/main.js
+++ server/main.js
@@ -7,8 +7,8 @@ Meteor.startup(function () {
   return myJobs.startJobServer();
 });
 
-// queue for jobs fo type 'echo'
-myJobs.processJobs(
+// Start the queue for jobs fo type 'echo'
+myJobs.processJobs( // TODO: shouldn't this run in startup?
   // Type of job to request
   // Can also be an array of job types
   'echo',
@@ -16,13 +16,14 @@ myJobs.processJobs(
     pollInterval: 1*1000, // Poll every 1 second
   },
   function (job, callback) {
-
     // ##### THIS IS WHERE THE WORK GETS DONE #####//
     console.log("job process triggered");
     // Only called when there is a valid job
+
+    //job.data is a data object the developer can pass when submitting up a new job.
     console.log( job.data );
 
-    job.done(); // mark the job done
+    job.done(); // Mark the job done
     callback(); // TODO: what is this? Can the dev modify this?
   }
 );
@@ -33,8 +34,7 @@ Meteor.methods({
 
     var newEchoJob = new Job(myJobs, //the job collection to use
       'echo', // type of job
-      // Job data that you define, including anything the job
-      // needs to complete. May contain links to files, etc...
+      // A data object that will be made available to the job worker. See 'job.data' above
       {
         message: 'Hello, is there anybody in here?'
       }
@@ -44,7 +44,7 @@ Meteor.methods({
     newEchoJob.priority('normal');
 
     newEchoJob.repeat({
-      repeats: 2, //the 'echo': repeats two more times in addition to the initial
+      repeats: 2, //the 'echo': repeats two more times in addition to the initial. This spawns new 'myJobs' tasks/docs
       wait: 10 //milliseconds TODO: why is this waiting 15 seconds between repeats? Is something blocking?
     });
 
